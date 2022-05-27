d1b52f9ae68bff7d88603a19588a0d7372759d43
anton-chertykovtsev <vader666@yandex.ru>
2022-05-28 00:49:25 +0300

    solution

--- cache.go
+++ cache.go
@@ -2,22 +2,43 @@ package cache
 
 import "time"
 
+type Item struct {
+	value    string
+	deadline time.Time
+}
+
 type Cache struct {
+	items map[string]Item
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{items: make(map[string]Item)}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	if item, ok := c.items[key]; ok {
+		if item.deadline.IsZero() || item.deadline.After(time.Now()) {
+			return item.value, true
+		}
+		return "", false
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.items[key] = Item{value, time.Time{}}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	var keys []string
+	for k, v := range c.items {
+		if v.deadline.IsZero() || v.deadline.After(time.Now()) {
+			keys = append(keys, k)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.items[key] = Item{value, deadline}
 }
